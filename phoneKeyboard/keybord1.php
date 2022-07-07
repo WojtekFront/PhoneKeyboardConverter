f72797e3d89bcf7265b3ebad702f4ca0129e38cc
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2022-07-07 23:18:23 +0200

    first committ
    first sharing with scratch of operation

---
+++ phoneKeyboard/keybord1.php
@@ -0,0 +1,57 @@
+<?php
+
+class PhoneKeyboardConverter{
+ 
+
+public $convert=[
+    1=>"a",11=>"b",111=>"c",
+    2=>"d",22=>"e",22=>"f",
+    3=>"g",33=>"h",333=>"i",
+    4=>"j",44=>"k",444=>"l",
+    5=>"m",55=>"n",555=>"o",
+    6=>"p",66=>"q",666=>"r",
+    7=>"s",77=>"t",777=>"u",
+    8=>"v",88=>"w",888=>"x",
+    9=>"y",99=>"z",999=>".",
+    0=>" ",00=>"+",000=>"*"
+];
+//check value empty - not goood, we want : string, number array
+function getValue($enteredValue){
+if(!isset($enteredValue)){ // "empty value"
+    
+    exit();
+}elseif(is_array($enteredValue)){ // if is array then send to "check first character"
+
+}elseif(is_int($enteredValue)){ // if is numeric then send to convertToString()
+
+}elseif(is_string($enteredValue)){ // if is string check if contains "," if not send to convertToNumeric()
+
+}else{ // "invalid value"
+    
+}
+}
+
+function convertToNumeric(){
+
+}
+function convertToString(){
+
+}
+
+}
+
+$tablicaDoZmianyNumery=[33,22,444,444,555];
+$tablicaDoZmianyTekst=["w","o","r","d"];
+
+
+?>
+<!-- weryfikacja:
+1. sprawdzamy co dostajemy i czy jest to tablica; //R
+2. sprawdzamy czy tablica jest pusta // R
+3. weryfikujemy pierwszy znak tablicy czy jest to nr lub litera plus spacja lub przecinek //
+4. jeżeli jest numer to sprawdzamy czy wszystkie znaki są numerami i analogicznie do litery plus spacja przecinek
+
+tłumaczenie:
+1. zlecamy tłumaczenie cyfr na znaki
+2. zlecamy tłumaczenie znaków na cyfry i drukujemy komunikat
+ -->
